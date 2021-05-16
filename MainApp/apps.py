2edda37afee2cbe1d326934ddeefd9b4e48e819f
aaadddiii <adithya.kurien@gmail.com>
2021-05-16 15:47:23 +0530

    some changes

--- MainApp/apps.py
+++ MainApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BixertWebsiteConfig(AppConfig):
+class MainappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bixert_website'
+    name = 'MainApp'
