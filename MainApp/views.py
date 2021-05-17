0ac0908dc0f4b48542662da3e7f7f0b64b1c6206
aaadddiii <adithya.kurien@gmail.com>
2021-05-17 15:07:04 +0530

    requirements update

--- MainApp/views.py
+++ MainApp/views.py
@@ -3,14 +3,9 @@ from .models import Event
 
 
 def home(request):
-<<<<<<< HEAD
     context = {"events": Event.objects.all()}
     return render(request, "MainApp/home.html", context)
-=======
-    return render(request, "MainApp/home.html")
+
 
 def events(request):
-    return render(request, "MainApp/events.html", {
-        
-    })
->>>>>>> 10b5af1f6eb793b519e479ba182c2873c2c449f8
+    return render(request, "MainApp/events.html", {})
