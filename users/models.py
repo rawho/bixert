a2542bc893552ef8217edcac439e9c7e839e0ce2
Rahul T <rahulmanoj@cet.ac.in>
2021-05-17 23:12:47 +0530

    navbar

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -7,6 +8,17 @@ class Profile(models.Model):
     image = models.ImageField(
         default="profile_pics/default.jpg", upload_to="profile_pics"
     )
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
     def __str__(self):
         return f"{self.user.username} Profile"
+
+    
