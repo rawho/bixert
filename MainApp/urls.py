f05f8d1e0b61a1b2c0ede40ce284caecb6a774f2
aaadddiii <adithya.kurien@gmail.com>
2021-05-18 16:55:20 +0530

    fixed register

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -3,8 +3,8 @@ from . import views
 from .views import EventsListView, EventsDetailView
 
 urlpatterns = [
-    path("events", EventsListView.as_view(), name="events"),
+    path("events/", EventsListView.as_view(), name="events"),
     path("events/<int:pk>/", EventsDetailView.as_view(), name="event-detail"),
     path("events/new/", views.createEvent, name="create-event"),
-    path("myevents", views.myevents, name="myevents")
+    path("myevents", views.myevents, name="myevents"),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,23 +1,32 @@
 from django.shortcuts import render, redirect
 from .models import Event
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, View
 from django.views.decorators.csrf import csrf_exempt
 from django.core.files.storage import FileSystemStorage
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
+
 def myevents(request):
-    return render(request, 'MainApp/events.html', {
-        "events" : Event.objects.filter(author=request.user.id)
-    })
+    return render(
+        request,
+        "MainApp/events.html",
+        {"events": Event.objects.filter(author=request.user.id)},
+    )
 
 
-
-class EventsListView(ListView):
+class EventsListView(ListView, View):
     model = Event
     template_name = "MainApp/events.html"  # <app>/<models>_<viewtype>.html
     context_object_name = "events"
     ordering = ["date_posted"]
+
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            template_name=self.template_name,
+            context={"events": Event.objects.all(), "request": request},
+        )
 
 
 class EventsDetailView(DetailView):
@@ -26,19 +35,26 @@ class EventsDetailView(DetailView):
 
 @csrf_exempt
 def createEvent(request):
-    if request.method == "POST" and request.FILES['banner']:
+    if request.method == "POST" and request.FILES["banner"]:
         title = request.POST.get("title")
         content = request.POST.get("content")
         author = request.user
-        date = request.POST.get('date')
-        max_participants = request.POST.get('maxParticipants')
-        location = request.POST.get('location')
-        if location == 'venue':
-            location = request.POST.get('place')
-        banner = request.FILES['banner']
+        date = request.POST.get("date")
+        max_participants = request.POST.get("maxParticipants")
+        location = request.POST.get("location")
+        if location == "venue":
+            location = request.POST.get("place")
+        banner = request.FILES["banner"]
         fs = FileSystemStorage()
         filename = fs.save(banner.name, banner)
-        Event.objects.create(title=title, content=content, author=author, banner=filename, venue=location, date_posted=date, max_participants=max_participants)
+        Event.objects.create(
+            title=title,
+            content=content,
+            author=author,
+            banner=filename,
+            venue=location,
+            date_posted=date,
+            max_participants=max_participants,
+        )
         return redirect("events")
     return render(request, "MainApp/createEvent.html", {})
- 
