ded774d2c1b678816b7eab084fb1011a3d6b7df2
aaadddiii <adithya.kurien@gmail.com>
2021-05-18 10:00:07 +0530

    adding logout

--- users/urls.py
+++ users/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
-urlpatterns = []
+urlpatterns = [path("logout/", views.logout_view, name="logout")]
--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm, LoginForm
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 
@@ -36,3 +36,8 @@ def register_login(request):
 @login_required
 def profile(request):
     return render(request, "users/profile.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("MainApp-home")
