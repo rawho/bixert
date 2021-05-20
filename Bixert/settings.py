cc3f4e99492e738862fbed678b415921bbf2c512
Rahul T <rahulmanoj@cet.ac.in>
2021-05-20 23:09:32 +0530

    update

--- Bixert/settings.py
+++ Bixert/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY") or os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,9 +84,6 @@ WSGI_APPLICATION = "Bixert.wsgi.application"
 
 
 # Database settings
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
 
 DATABASES = {
     'default': {
@@ -99,6 +96,9 @@ DATABASES = {
     }
 }
 
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -146,6 +146,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = "587"
-EMAIL_HOST_USER = env("email_username")
-EMAIL_HOST_PASSWORD = env("email_password")
+EMAIL_HOST_USER = env("email_username") or os.environ.get("EMAIL_USERNAME")
+EMAIL_HOST_PASSWORD = env("email_password") or os.environ.get("EMAIL_PASSWORD")
 EMAIL_USE_TLS = True
