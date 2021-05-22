97637042afe590102bf24e5092e65882f9c10f1e
aaadddiii <adithya.kurien@gmail.com>
2021-05-22 17:33:28 +0530

    changes

--- MainApp/send_mail.py
+++ MainApp/send_mail.py
@@ -9,25 +9,25 @@ def sendmail(event, user):
     sender_email = os.environ.get("EMAIL_USERNAME")
     receiver_email = user.email
     password = os.environ.get("EMAIL_PASSWORD")
-    # Create the plain-text and HTML version of your message
-    text = """\
-    Hi,
-    How are you?
-    Real Python has many great tutorials:
-    www.realpython.com"""
-    html = f"""\
-    <html>
-    <body>
-        <div style="width:80%; margin: auto;">
-            <h1>{event.title} </h1>
-            <p>{event.content} </p>
-            <p>Are you coming?</p>
-            <a href="https://bixert.xyz/verify/{event.id}-{user.id}"><button>Yes</button></a> <button>No</button>
-        </div>
-    </body>
-    </html>
-    """
-    message_body = html
+    # # Create the plain-text and HTML version of your message
+    # text = """\
+    # Hi,
+    # How are you?
+    # Real Python has many great tutorials:
+    # www.realpython.com"""
+    # html = f"""\
+    # <html>
+    # <body>
+    #     <div style="width:80%; margin: auto;">
+    #         <h1>{event.title} </h1>
+    #         <p>{event.content} </p>
+    #         <p>Are you coming?</p>
+    #         <a href="https://bixert.xyz/verify/{event.id}-{user.id}"><button>Yes</button></a> <button>No</button>
+    #     </div>
+    # </body>
+    # </html>
+    # """
+    message_body = "hellooo please confirm invitation" 
     send_mail(
             "Verify Email", #subject
             message_body, #message
