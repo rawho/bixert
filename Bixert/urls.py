508f12e1bcb3be1c81ed206ec0fe7ceb7a49a192
aaadddiii <adithya.kurien@gmail.com>
2021-05-24 17:39:22 +0530

    password reset

--- Bixert/urls.py
+++ Bixert/urls.py
@@ -26,9 +26,22 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("profile/", user_views.profile, name="profile"),
-    path("", include("MainApp.urls")),
     path("app/", include("users.urls")),
     path("", user_views.register_login, name="MainApp-home"),
+    path("", user_views.register_login, name="login"),
+    path("password-reset/",
+    auth_views.PasswordResetView.as_view(template_name="users/password_reset.html"),
+    name="password_reset"),
+    path("password-reset/done/",
+    auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"),
+    name="password_reset_done"),
+    path("password-reset-confirm/<uidb64>/<token>/",
+    auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"),
+    name="password_reset_confirm"),
+    path("password-reset-complete/",
+    auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"),
+    name="password_reset_complete"),
+    path("", include("MainApp.urls")),
 ]
 
 if settings.DEBUG:
